86459ad0852bf5ebd9701ce9034bf65c95a068b2
Alex <alexbelka1992@mail.ru>
2020-07-28 01:43:57 +0300

    Планировщик задач

---
+++ js/action.js
@@ -0,0 +1,11 @@
+// let btn = document.querySelector('.btn');
+// let array = [];
+//
+// btn.onclick = function(){
+//     let group = document.querySelectorAll('.menu-item');
+//
+//
+//
+// };
+
+
---
+++ js/index.js
@@ -0,0 +1,27 @@
+let text = document.getElementById('text');
+let form = document.querySelector('.content-form');
+let menu = document.querySelector('.content-menu');
+let submit = document.querySelector('#submit');
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+let element = document.createElement('li');
+element.innerHTML = text.value;
+menu.append(element);
+text.value ='';
+
+let amount = menu.children;
+if(amount.length>=10){
+   text.setAttribute('disabled','disabled');
+   submit.setAttribute('disabled', 'disabled');
+};
+
+for(let i = 0; i<=amount.length; i++){
+    amount[i].classList.add('menu-item');
+    amount[i].onclick = function(){
+        amount[i].remove();
+    }
+}
+
+});
